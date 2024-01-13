739daf914b090334e600cca239479ee8fa958e2f
JyArci08 <154620020+JyArci08@users.noreply.github.com>
2024-01-13 02:42:14 +0000

    In progress Guided 2

--- 2. Control Structures/index.js
+++ 2. Control Structures/index.js
@@ -6,40 +6,66 @@
 const randomNumber = Math.floor((Math.random() * 100) + 1); // feel free to mock this value for testing
 
 // TODO 2.1 Display "more than fifty" if randomNumber is more than fify
-// Your code here
+if (randomNumber > 50) {
+    console.log("more than fifty");
+} else {console.log("not more than fifty");}
 
 // TODO 2.2 Display whether the random number is odd or even
-// Your code here
+if (randomNumber % 2 == 0) {
+    console.log("Even")
+} else {
+    console.log("Odd")
+}
 
 // TODO 2.3 If the number if a multiple of 3, write "fizz".
 // if the number is a multiple of 5 display "buzz".
 // if the number is divisible by both 3 and 5, display "fizzbuzz". otherwise, display the number
-// Your code here
+if (randomNumber % 3 == 0 && randomNumber % 5 == 0) {
+    console.log("fizzbuzz")
+} else if (randomNumber % 3 == 0) {
+    console.log("fizz")
+} else if (randomNumber % 5 == 0) {
+    console.log("buzz")
+}
+else {
+    console.log(randomNumber)
+}
 
 let toDisplay = ""
 // TODO 2.4 Use the ternary conditional operator to set toDisplay to "Even" if randomNumber is even and "Odd" if randomNumber is odd
-// Your code here
+toDisplay = (randomNumber % 2 == 0) ? "Even":"Odd"
 console.log("toDisplay", toDisplay);
 
 // Checkpoint 2.1 How do you use switch statements and when would you use them? Try researching the answer
-// Answer: 
+// Answer: Basically, i think the switch statements is a more organize looking than if else statement but they do the same thing.
+// We can use switch statement if we have many value to display for one condition.
 
 // TODO 2.5 Use a for loop to print the numbers 1 to N
-const n = 10
-// Your code here
+const nums = 10
+for (let i=1; i<=nums; i++) {
+    console.log(i);
+  }
 
 const list = ["apple", "banana", "cherry", "date", "elderberry"];
 // TODO 2.6 Use a while loop to display all the values in the list
-// Your code here
+let i = 0;
+while (list[i]) {
+    console.log(list[i])
+    i++;
+}
 
 // Checkpoint 2.3 How would you simulate a do-while loop in JavaScript
 // Answer: 
 
 // TODO 2.7 Use a for of loop to display all the values in the list
-// Your code here
+for (const i in list) {
+    console.log(list[i]);
+  }
 
 // TODO 2.8 Use a for in loop to display all the values in the list
-// Your code here
+for (const i in list) {
+    console.log(list[i]);
+  }
 
 // TODO 2.9 Use the for each method of the list to display all its values 
 // Your code here
